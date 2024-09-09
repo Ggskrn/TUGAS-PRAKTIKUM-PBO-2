a1e9ed0475bb62201ac8bef8a2aad2db49179f20
Ggskrn <148623788+Ggskrn@users.noreply.github.com>
2024-09-09 19:54:08 +0700

    Bernyanyi.java

---
+++ Bernyanyi.java
@@ -0,0 +1,17 @@
+jetbrains://idea/navigate/reference?project=TugasPBO2_GAGAS&fqn=BingoDog_Gagas3F.Bernyanyi
+C:\Users\USER\IdeaProjects\TugasPBO2_GAGAS\src\BingoDog_Gagas3F\Bernyanyi.java
+
+  package BingoDog_Gagas3F;
+
+public class Bernyanyi {
+    public static void main(String[] args) {
+        LaguBingo laguBingo = new LaguBingo(
+                "There was a farmer who had a dog,",
+                "And Bingo was his name-o.",
+                "And Bingo was his name-o.");
+            Bernyanyi.cetakLagu();
+    }
+
+    public static void cetakLagu() {
+    }
+}
