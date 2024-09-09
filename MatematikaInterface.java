dc0faf05a609dbb686b201c9e0d219a5713b91e3
Ggskrn <148623788+Ggskrn@users.noreply.github.com>
2024-09-09 19:36:45 +0700

    MatematikaInterface.java

---
+++ MatematikaInterface.java
@@ -0,0 +1,11 @@
+jetbrains://idea/navigate/reference?project=TugasPBO2_GAGAS&fqn=Matematika_Gagas3F.MatematikaInterface
+C:\Users\USER\IdeaProjects\TugasPBO2_GAGAS\src\Matematika_Gagas3F\MatematikaInterface.java
+
+  package Matematika_Gagas3F;
+
+public interface MatematikaInterface {
+    void pertambahan(int a, int b);
+    void pengurangan(int a, int b);
+    void perkalian(int a, int b);
+    void pembagian(int a, int b);
+}
